641c31e58d8254b926d295232f4527460e9b0dcb
John Marty <JohnRmarty@gmail.com>
2015-02-22 14:53:32 -0700

    change routes for get session login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ resources :users
 resources :manufacturers do
 resources :products
 
+get 'login' => 'sessions#new'
+get 'logout' => 'sessions#destroy'
+
+get 'users/new'
+get 'about/index'
+
+match ':controller(/:action(/:id))', :via => [:get, :post]
+
+end 
 end 
 
